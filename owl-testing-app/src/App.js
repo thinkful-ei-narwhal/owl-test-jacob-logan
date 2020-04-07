0a002684cea2034d494c3771652ca2e37a956948
Jacob Flaxman <jacobflaxman1@gmail.com>
2020-04-07 13:34:46 -0800

    fini

--- owl-testing-app/src/App.js
+++ owl-testing-app/src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 import Party from "./Part";
 import Stage from "./Stage";
 import Chat from "./Chat";
-// console.log(obj);
+
+
 
 export default function App(props) {
   // console.log(props.data);
@@ -27,19 +28,37 @@ export default function App(props) {
       onStage={val.onStage}
     />
   ))
-  const chatComponents = props.data.participants.map(val => (
+
+  let chatComponents = props.data.chatEvents.map(val => (
     <Chat
-      key={val.id}
-      avatar={val.avatar}
-      name={val.name}
-      onStage={val.onStage}
+      key={val.participantId}
+      type={val.type}
+      message ={val.message}
+      time={val.time}
+      timestamp = {val.timestamp}
     />
   ))
+
+    let chatID = 0;
+    for (let i = 0; i < props.data.participants.length; i++) {
+      console.log(props.data.participants[i])
+
+        // if(props.data.participants[i] === props.data.chatEvents) {
+        //   console.log(chatID)
+        //   chatID = props.data.participants[i]
+        }
+
+
+   console.log(chatID)
+
+
   return (
     <div className="App">
-      <div className="partyCSS">{components}</div>
+
+      <div className="chatCSS" >{chatComponents}</div>
+      <div className="partyCSS" >{components}</div>
       <div className="stageCSS">{stageComponents}</div>
-      <div className="chatCSS">{chatComponents}</div>
+      
     </div>
   );
 
--- owl-testing-app/src/chat.js
+++ owl-testing-app/src/chat.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import './chat.css'
 
 
-
-export default function (props) {
+const Chat = function (props) {
   return (
-    <div className="chat" key={props.id}>
-      <img src={props.avatar} alt="" />
+    <div className="chat" key={props.participantId}>
       <div className="details">
-        <div className="participantName">{props.name}</div>
-        <div className="status">
-          {props.onStage ? "On Stage" : "In Session"}
-        </div>
+        <div className = 'findName'> {props.name} </div>
+        <div className="type">{props.type}</div>
+        <div className="message"> {props.message} </div>
+        <div className = 'time' style={{ display: (props.type === 'message' ? 'flex' : 'none') }}> {Date(props.time)} </div>
+        <div className = 'timeStamp' style={{ display: 'none' }}> {Date(props.timestamp)} </div>
       </div>
     </div>
   );
 }
+
+
+export default Chat;
